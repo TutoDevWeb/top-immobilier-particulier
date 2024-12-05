5b87890a46cc4505052fd6d34a089b7f52e1e6ff
TutoDevWeb <TutoDevWeb@outlook.fr>
2024-12-05 17:48:22 +0100

    Correction de la déclaration des constantes. Modification des msqli avec le passage de $connexion. Modif à poursuivre.

--- data/data.php
+++ data/data.php
@@ -1,8 +1,8 @@
 <?PHP
 
-define(SAG_SERVER, '149.202.50.161');
+define('SAG_SERVER', '149.202.50.161');
 
-define(SAG_SQL_SERVER, 'localhost');
-define(SAG_SQL_USER, 'user_tipfr');
-define(SAG_SQL_PASSWORD, 'pass_tipfr');
-define(SAG_SQL_DATABASE, 'dtb_tipfr');
+define('SAG_SQL_SERVER', 'localhost');
+define('SAG_SQL_USER', 'user_tipfr');
+define('SAG_SQL_PASSWORD', 'pass_tipfr');
+define('SAG_SQL_DATABASE', 'dtb_tipfr');
--- include/inc_base.php
+++ include/inc_base.php
@@ -1,28 +1,26 @@
 <?PHP
 //-------------------------------------------------------------------------------------
-function dtb_connection(){
+function dtb_connection() {
 
-  $the_mysqli_server   = SAG_SQL_SERVER ;
-  $the_mysqli_user     = SAG_SQL_USER;
-  $the_mysqli_password = SAG_SQL_PASSWORD;
-  $the_mysqli_database = SAG_SQL_DATABASE;
+	$the_mysqli_server   = SAG_SQL_SERVER;
+	$the_mysqli_user     = SAG_SQL_USER;
+	$the_mysqli_password = SAG_SQL_PASSWORD;
+	$the_mysqli_database = SAG_SQL_DATABASE;
 
-  if ( $base = mysqli_connect($the_mysqli_server,$the_mysqli_user,$the_mysqli_password)) {
-    mysqli_select_db($the_mysqli_database,$base);
-  }
+	if ($connexion = mysqli_connect($the_mysqli_server, $the_mysqli_user, $the_mysqli_password)) {
+		mysqli_select_db($connexion, $the_mysqli_database);
+		return $connexion;
+	}
 }
 //-------------------------------------------------------------------------------------
-function dtb_query($query,$file,$line,$trace) {
-  if ( $result = mysqli_query($query)) {
+function dtb_query($connexion, $query, $file, $line, $trace) {
+	if ($result = mysqli_query($connexion, $query)) {
 		if ($trace) echo "<p>OK:$query</p>\n";
-    return $result;
-  } else {
+		return $result;
+	} else {
 		if ($trace) echo "<p>KO:$query</p>\n";
-    $err = addslashes('KO: Erreur n� :mysqli_l_errno().' : mysqli_l_error().'<br/>');
-    echo "$err<br/>$line:$file<br/>\n";
-    return false;
-  }
+		$err = addslashes('KO: Erreur numéro :mysqli_l_errno()' . mysqli_error($connexion) . '<br/>');
+		echo "$err<br/>$line:$file<br/>\n";
+		return false;
+	}
 }
-
-
-?>
