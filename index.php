fd7b47ba1fd831bec1c1a14d89cba0bcc6de160b
TutoDevWeb <TutoDevWeb@outlook.fr>
2024-12-05 17:53:35 +0100

    Correction du fichier. inc_conf.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?PHP
 include("data/data.php");
 include("include/inc_base.php");
-// include("include/inc_conf.php");
+include("include/inc_conf.php");
 // include("include/inc_format.php");
 // include("include/inc_diaporama.php");
 // include("include/inc_random.php");
