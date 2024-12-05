1ed10c07ffa865e25a415ccdf96b0fc9619fdb7f
TutoDevWeb <TutoDevWeb@outlook.fr>
2024-12-05 18:11:51 +0100

    Modif de inc_vep.php

--- include/inc_vep.php
+++ include/inc_vep.php
@@ -1,14 +1,7 @@
 <?PHP
 function print_vep_logo() {
-?>
-<a href="http://www.villas-en-provence.com" title="Villas en Provence Agences et Particuliers"> 
-<img src="http://www.villas-en-provence.com/vlp/logo/villas-provence-88x31.gif" alt="Villas en Provence Agences et Particuliers" /></a> 
-<?PHP
+	echo "Print_vep_logo<br>";
 }
 function print_vep_ban() {
-
-  echo "<div id='vepban'><a href='http://www.villas-en-provence.com' title='Villas en Provence Agences et Particuliers'>\n";
-  echo "<img src='/images-pub/villas-en-provence-428x60-1.gif' alt='villas en provence' /></a></div>\n";
-
+	echo "Print_vep_ban<br>";
 }
-?>
