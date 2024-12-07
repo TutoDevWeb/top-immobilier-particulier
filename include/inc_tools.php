2f4bb730575c8641ffbb40a52aa3ea82e893de31
TutoDevWeb <TutoDevWeb@outlook.fr>
2024-12-07 11:10:23 +0100

    Neutralisation de Xiti

--- include/inc_tools.php
+++ include/inc_tools.php
@@ -20,9 +20,9 @@ function print_tools($type = '') {
 	?>
 		<div id='toolscenter'>
 			<ul id='tools'>
-				<li><a href='http://www.top-immobilier-particulier.fr/' title="Aller � l'accueil">www.top-immobilier-particulier.fr</a>&nbsp;|&nbsp;</li>
+				<li><a href='http://www.top-immobilier-particulier.fr/' title="Aller à l'accueil">www.top-immobilier-particulier.fr</a>&nbsp;|&nbsp;</li>
 				<li><a href='http://www.top-immobilier-particulier.fr/plan-du-site.htm' title='Voir le Plan du site'>Plan du site</a>&nbsp;|&nbsp;</li>
-				<li><a href='http://www.top-immobilier-particulier.fr/noref/faq.php' title='R�ponses aux questions fréquemment posées'>FAQ</a>&nbsp;|&nbsp;</li>
+				<li><a href='http://www.top-immobilier-particulier.fr/noref/faq.php' title='Réponses aux questions fréquemment posées'>FAQ</a>&nbsp;|&nbsp;</li>
 				<li><a href='http://www.top-immobilier-particulier.fr/noref/contact-equipe.php?action=print_form' title='Contacter nous' rel="nofollow">Contacter Nous</a>&nbsp;|&nbsp;</li>
 				<li><a href='http://www.top-immobilier-particulier.fr/noref/loupe.php' title='Augmenter ou diminuer la taille du texte'>+ Loupe -</a>&nbsp;|&nbsp;</li>
 				<li><a href='http://lexique.top-immobilier-particulier.fr' title='Lexique Immobilier'>Lexique Immobilier</a></li>
--- include/inc_xiti.php
+++ include/inc_xiti.php
@@ -1,36 +1,4 @@
 <?PHP
-// Fonction pour impression du code Xiti + Google Analytics
 function print_xiti_code($page) {
-?>
-<div id="xiti-logo">
-<a href="http://www.xiti.com/xiti.asp?s=370898" title="WebAnalytics" target="_top">
-<script type="text/javascript">
-<!--
-Xt_param = 's=370898&p=';
-try {Xt_r = top.document.referrer;}
-catch(e) {Xt_r = document.referrer; }
-Xt_h = new Date();
-Xt_i = '<img width="80" height="15" border="0" alt="" ';
-Xt_i += 'src="http://logv3.xiti.com/bcg.xiti?'+Xt_param;
-Xt_i += '&hl='+Xt_h.getHours()+'x'+Xt_h.getMinutes()+'x'+Xt_h.getSeconds();
-if(parseFloat(navigator.appVersion)>=4)
-{Xt_s=screen;Xt_i+='&r='+Xt_s.width+'x'+Xt_s.height+'x'+Xt_s.pixelDepth+'x'+Xt_s.colorDepth;}
-document.write(Xt_i+'&ref='+Xt_r.replace(/[<>"]/g, '').replace(/&/g, '$')+'" title="Internet Audience">');
-//-->
-</script>
-<noscript>
-Mesure d'audience ROI statistique webanalytics par <img width="80" height="15" src="http://logv3.xiti.com/bcg.xiti?s=370898&p=" alt="WebAnalytics" />
-</noscript></a>
-</div>
-
-<!--
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-1046103-2";
-urchinTracker();
-</script>
--->
-<?PHP
+	return;
 }
-?>
