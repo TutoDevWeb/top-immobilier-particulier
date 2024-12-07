ce5d2b6c116db547ab32d8ea367937f8d12af1c9
TutoDevWeb <TutoDevWeb@outlook.fr>
2024-12-07 19:33:33 +0100

    Correction des Javascript

--- compte-annonce/jvscript/valid_form_ano.js
+++ compte-annonce/jvscript/valid_form_ano.js
@@ -1,80 +1,80 @@
 //---------------------------------------------------------------------
 function valid_form_ano(zone) {
 
-  var debug = 1;
-  //if ( debug) window.alert(zone);
+	var debug = 1;
+	//if ( debug) window.alert(zone);
 
-  // Premier Tableau
-  var zone_pays  = document.getElementById("zone_pays");
-  var zone_ville = document.getElementById("zone_ville");
-  var num_dept   = document.getElementById("num_dept");
-  var zone_dom   = document.getElementById("zone_dom");
-  var zone_ard   = document.getElementById("zone_ard");
-	
-  if ( is_empty(zone_pays,"Pays ?")) return false;
+	// Premier Tableau
+	var zone_pays = document.getElementById("zone_pays");
+	var zone_ville = document.getElementById("zone_ville");
+	var num_dept = document.getElementById("num_dept");
+	var zone_dom = document.getElementById("zone_dom");
+	var zone_ard = document.getElementById("zone_ard");
 
-  // Dans ce cas on doit vérifier la Ville et le Département de Métropole
-  if ( zone == 'france' ) {
-    //if ( debug) window.alert('Vérification FRANCE');
-    if ( is_unselected(num_dept,"Choisir le Département !")) return false;
-    if ( is_unselected(zone_ville,"Choisir la Ville !")) return false;
-    if ( zone_ville.value == 'Paris' || zone_ville.value == 'Marseille' || zone_ville.value == 'Lyon' ) {
-      if ( is_unselected(zone_ard,"Choisir l'Arrondissement !")) return false;
+	if (is_empty(zone_pays, "Pays ?")) return false;
+
+	// Dans ce cas on doit vÃ©rifier la Ville et le DÃ©partement de MÃ©tropole
+	if (zone == 'france') {
+		//if ( debug) window.alert('VÃ©rification FRANCE');
+		if (is_unselected(num_dept, "Choisir le DÃ©partement !")) return false;
+		if (is_unselected(zone_ville, "Choisir la Ville !")) return false;
+		if (zone_ville.value == 'Paris' || zone_ville.value == 'Marseille' || zone_ville.value == 'Lyon') {
+			if (is_unselected(zone_ard, "Choisir l'Arrondissement !")) return false;
 		}
 	}
-	
-  // Dans ce cas on doit vérifier la Ville et la Département Hors Métropole
-  if ( zone == 'domtom' ) {
-    //if ( debug) window.alert('Vérification DOM TOM');
-    if ( is_unselected(zone_dom,"Sélectionner le Département ?")) return false;
-    if ( is_empty(zone_ville,"Ville ?")) return false;	
+
+	// Dans ce cas on doit vÃ©rifier la Ville et la DÃ©partement Hors MÃ©tropole
+	if (zone == 'domtom') {
+		//if ( debug) window.alert('VÃ©rification DOM TOM');
+		if (is_unselected(zone_dom, "SÃ©lectionner le DÃ©partement ?")) return false;
+		if (is_empty(zone_ville, "Ville ?")) return false;
 	}
 
-  // Dans ce cas on doit vérifier la Ville
-  if ( zone == 'etranger' ) {
-    if ( is_empty(zone_ville,"Ville ?")) return false;	
+	// Dans ce cas on doit vÃ©rifier la Ville
+	if (zone == 'etranger') {
+		if (is_empty(zone_ville, "Ville ?")) return false;
 	}
 
-  // Deuxième Tableau
-	var typp       = document.getElementById("typp");
-	var nbpi       = document.getElementById("nbpi");
-	var surf       = document.getElementById("surf");
-	var prix       = document.getElementById("prix");
+	// DeuxiÃ¨me Tableau
+	var typp = document.getElementById("typp");
+	var nbpi = document.getElementById("nbpi");
+	var surf = document.getElementById("surf");
+	var prix = document.getElementById("prix");
 
-  if ( is_unselected(typp,"Type de Produit ?")) return false;
-  if ( is_unselected(nbpi,"Nombre de Pièces ?")) return false;
-  if ( is_empty(surf,"Surperficie ?")) return false;
-  if ( is_empty(prix,"Prix ?")) return false;
+	if (is_unselected(typp, "Type de Produit ?")) return false;
+	if (is_unselected(nbpi, "Nombre de PiÃ¨ces ?")) return false;
+	if (is_empty(surf, "Surperficie ?")) return false;
+	if (is_empty(prix, "Prix ?")) return false;
 
-  // Troisième Tableau
+	// TroisiÃ¨me Tableau
 	var blabla = document.getElementById("blabla");
-  if ( is_empty(blabla,"Description de votre produit ?")) return false;
+	if (is_empty(blabla, "Description de votre produit ?")) return false;
 
-  // Quatrième Tableau
-	var tel_ins   = document.getElementById("tel_ins");
-	var tel_bis   = document.getElementById("tel_bis");
-	var sagmail   = document.getElementById("sagmail");
+	// QuatriÃ¨me Tableau
+	var tel_ins = document.getElementById("tel_ins");
+	var tel_bis = document.getElementById("tel_bis");
+	var sagmail = document.getElementById("sagmail");
 	var condition = document.getElementById("condition");
-	var code_set  = document.getElementById("code_set");
-	var code_get  = document.getElementById("code_get");
+	var code_set = document.getElementById("code_set");
+	var code_get = document.getElementById("code_get");
 
-  if ( is_empty(tel_ins,"Votre Téléphone 1 ?")) return false;
-  if ( telephone_invalide(tel_ins) ) return false;
-	
-  if ( is_empty(sagmail,"Votre Email ?")) return false;
-  if ( email_invalide(sagmail) ) return false;
-	
-	// Si le téléphone bis est renseigné, le vérifier
-	if ( tel_bis.value != "" && telephone_invalide(tel_bis) ) return false;
+	if (is_empty(tel_ins, "Votre TÃ©lÃ©phone 1 ?")) return false;
+	if (telephone_invalide(tel_ins)) return false;
 
-  if ( code_get_empty(code_get) ) return false;
-  if ( code_get_false(code_set,code_get) ) return false;
-	
-	// Vérifier que les conditions sont lues
-	if ( condition.checked != true ) {
-	  window.alert("Lire les conditions d'utilisation et cocher SVP");
+	if (is_empty(sagmail, "Votre Email ?")) return false;
+	if (email_invalide(sagmail)) return false;
+
+	// Si le tÃ©lÃ©phone bis est renseignÃ©, le vÃ©rifier
+	if (tel_bis.value != "" && telephone_invalide(tel_bis)) return false;
+
+	if (code_get_empty(code_get)) return false;
+	if (code_get_false(code_set, code_get)) return false;
+
+	// VÃ©rifier que les conditions sont lues
+	if (condition.checked != true) {
+		window.alert("Lire les conditions d'utilisation et cocher SVP");
 		return false;
-  }
+	}
 
 	return true;
 }
--- compte-annonce/jvscript/valid_form_connexion.js
+++ compte-annonce/jvscript/valid_form_connexion.js
@@ -1,16 +1,16 @@
 //---------------------------------------------------------------------
 function valid_form_connexion() {
 
-  var compte_tel_ins  = document.getElementById("connexion_compte_tel_ins");
-  var compte_pass   = document.getElementById("connexion_compte_pass");
-	
-  if ( is_empty(compte_tel_ins,"Merci de mettre votre numéro de téléphone numéro 1.\n\nVous nous avez donné ce numéro au moment de la création de l'annonce\n\nVous le trouverez dans le mail que vous avez dû recevoir au moment de la création de l'annonce")) return false;
+	var compte_tel_ins = document.getElementById("connexion_compte_tel_ins");
+	var compte_pass = document.getElementById("connexion_compte_pass");
 
-	// Vérifier sagpass
-  reg_pass = RegExp("[a-zA-Z0-9]{5}");
-	if ( !reg_pass.test(compte_pass.value) ) {
-	  window.alert("Nous détectons une erreur de format sur le mot de passe que vous nous proposez.\n\nMerci de mettre le mot de passe qui vous a été attribué.\n\nVous le trouverez dans le mail que vous avez dû recevoir au moment de la création de l'annonce");
-    return false;
+	if (is_empty(compte_tel_ins, "Merci de mettre votre numÃ©ro de tÃ©lÃ©phone numÃ©ro 1.\n\nVous nous avez donnÃ© ce numÃ©ro au moment de la crÃ©ation de l'annonce\n\nVous le trouverez dans le mail que vous avez dÃ» recevoir au moment de la crÃ©ation de l'annonce")) return false;
+
+	// VÃ©rifier sagpass
+	reg_pass = RegExp("[a-zA-Z0-9]{5}");
+	if (!reg_pass.test(compte_pass.value)) {
+		window.alert("Nous dÃ©tectons une erreur de format sur le mot de passe que vous nous proposez.\n\nMerci de mettre le mot de passe qui vous a Ã©tÃ© attribuÃ©.\n\nVous le trouverez dans le mail que vous avez dÃ» recevoir au moment de la crÃ©ation de l'annonce");
+		return false;
 	}
 
 	return true;
@@ -18,9 +18,9 @@ function valid_form_connexion() {
 //---------------------------------------------------------------------
 function valid_form_password() {
 
-  var compte_tel_ins  = document.getElementById("password_compte_tel_ins");
-	
-  if ( is_empty(compte_tel_ins,"Merci de mettre votre numéro de téléphone numéro 1.\n\nVous nous avez donné ce numéro au moment de la création de l'annonce\n\nVous le trouverez dans le mail que vous avez dû recevoir au moment de la création de l'annonce")) return false;
+	var compte_tel_ins = document.getElementById("password_compte_tel_ins");
+
+	if (is_empty(compte_tel_ins, "Merci de mettre votre numÃ©ro de tÃ©lÃ©phone numÃ©ro 1.\n\nVous nous avez donnÃ© ce numÃ©ro au moment de la crÃ©ation de l'annonce\n\nVous le trouverez dans le mail que vous avez dÃ» recevoir au moment de la crÃ©ation de l'annonce")) return false;
 	return true;
 
 }
